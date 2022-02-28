9e6126c0e8ffe51e4e94b67937455be97f45faa1
joseph <arias.joseph.a@gmail.com>
2022-02-28 02:19:53 -0500

    using UseEffect hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import StateTutorial from './StateTutorial';
 import ReducerTutorial from './ReducerTutorial';
+import EffectTutorial from './EffectTutorial';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      {/* <StateTutorial /> */}
-    <ReducerTutorial></ReducerTutorial>
+    {/* <ReducerTutorial></ReducerTutorial> */}
+    <EffectTutorial></EffectTutorial>
     </div>
   );
 }
---
+++ src/EffectTutorial.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+function EffectTutorial() {
+
+  const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/comments")
+    .then(response => {
+      console.log(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      Hello World
+      <h1>{data}</h1>
+      <h1>{count}</h1>
+
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >Click</button>
+    </div>
+  )
+}
+
+export default EffectTutorial;
